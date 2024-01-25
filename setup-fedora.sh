a0adf99d2f72e29ff3d2edec3d9294f411a04a56
Yago Alexandre <77808082+yagoalexandre00@users.noreply.github.com>
2024-01-25 20:29:54 -0300

    Update setup-fedora.sh

--- setup-fedora.sh
+++ setup-fedora.sh
@@ -74,3 +74,11 @@ rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 dnf check-update -y
 dnf install -y code
+
+# Codecs
+dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-plugin-libav --exclude=gstreamer1-plugins-bad-free-devel -y
+dnf install lame\* --exclude=lame-devel -y
+dnf group upgrade --with-optional Multimedia
+
+# Tilix
+dnf install -y tilix
